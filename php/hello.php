f0272c53025f214a4467cdcba8741d5a847494c8
Koki Yoshida <kokiyoshida@KokiYoshida-no-MacBook-Air.local>
2015-08-08 02:16:42 +0900

    miner change

--- php/hello.php
+++ php/hello.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset='utf-8'>
-<title>PHP test</title>
-</head>
-<body>
+    <head>
+        <meta charset='utf-8'>
+        <title>PHP test</title>
+    </head>
+    <body>
 <?php
 echo "<p>Hello PHP World!</p>";
 ?>
-</body>
+    </body>
 </html>
